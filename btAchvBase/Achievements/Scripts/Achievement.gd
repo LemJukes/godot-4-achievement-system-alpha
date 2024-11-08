4ed05b4c6101e300a5e289111b4b516735d5b6bb
Rob Garner <jukeLTD@proton.me>
2024-11-08 17:33:34 -0500

    ugh

--- btAchvBase/Achievements/Scripts/Achievement.gd
+++ btAchvBase/Achievements/Scripts/Achievement.gd
@@ -1,60 +1,54 @@
 extends Node
 
-var values = {};
+var values: Dictionary = {}
 
-func _init(values):
-	self.values = values;
+func _init(initial_values: Dictionary) -> void:
+    values = initial_values
 
-func set_value(key, value):
-	values[key][key] = value;
+func set_value(key: String, value) -> void:
+    if key in values:
+        values[key][key] = value
 
-func get_value(key):
-	return values[key][key];
+func get_value(key: String):
+    if key in values:
+        return values[key][key]
+    return null
 
-func is_key_user_data(key):
-	if values[key].has("userData"):
-		return values[key]["userData"];
-	
-	return false;
+func is_key_user_data(key: String) -> bool:
+    if key in values and "userData" in values[key]:
+        return values[key]["userData"]
+    return false
 
-func get_name():
-	if get_value("name") != null:
-		return get_value("name");
-	
-	return "Name Not Set";
+func get_achvName() -> String:
+    var name = get_value("achvName")
+    return name if name != null else "Name Not Set"
 
-func get_desc():
-	if get_value("desc") != null:
-		return get_value("desc");
-	
-	return "Desc Not Set";
+func get_desc() -> String:
+    var desc = get_value("desc")
+    return desc if desc != null else "Desc Not Set"
 
-func get_progress():
-	if get_value("progress") != null:
-		return get_value("progress");
-	
-	return 0;
-	
-func get_total():
-	if get_value("total") != null:
-		return get_value("total");
-	
-	return 1;
+func get_progress() -> int:
+    var progress = get_value("progress")
+    return progress if progress != null else 0
 
-func has_custom_progress_bar_colors():
-	return values.has("progress-bar-background-color") && values.has("progress-bar-color");
+func get_total() -> int:
+    var total = get_value("total")
+    return total if total != null else 1
 
-func get_progress_bar_background_color():
-	return Color(get_value("progress-bar-background-color"));
+func has_custom_progress_bar_colors() -> bool:
+    return "progress-bar-background-color" in values and "progress-bar-color" in values
 
-func get_progress_bar_color():
-	return Color(get_value("progress-bar-color"));
+func get_progress_bar_background_color() -> Color:
+    return Color(get_value("progress-bar-background-color"))
 
-func has_picture():
-	return values.has("picture");
+func get_progress_bar_color() -> Color:
+    return Color(get_value("progress-bar-color"))
 
-func get_picture_location():
-	return get_value("picture");
+func has_picture() -> bool:
+    return "picture" in values
 
-func increment(amount):
-	values["progress"]["progress"] = get_value("progress") + amount;
+func get_picture_location() -> String:
+    return get_value("picture")
+
+func increment(amount: int) -> void:
+    values["progress"]["progress"] = get_value("progress") + amount
