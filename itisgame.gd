11c5e8d5934ef8796e5313e6d95a006db5984f50
Rob Garner <jukeLTD@proton.me>
2024-11-08 14:13:52 -0500

    Update itisgame.gd

--- itisgame.gd
+++ itisgame.gd
@@ -5,8 +5,10 @@ var score: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	score_value.text = str(score)
+	pass
 
 func _on_score_button_pressed() -> void:
 	score += 1
 	score_value.text = str(score)
+	print("Score: ", score)
+	
